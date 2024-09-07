a53441c9841ee591172cfcb015f0d4eaa1d5d7f2
Punyapal Shah <mrpunyapal@gmail.com>
2024-09-07 17:07:13 +0530

    feat: added follow button in users component

--- resources/views/livewire/home/users.blade.php
+++ resources/views/livewire/home/users.blade.php
@@ -23,12 +23,12 @@
         <section class="max-w-2xl">
             <ul class="flex flex-col gap-2">
                 @foreach ($users as $user)
-                    <li>
-                        <a
-                            href="{{ route('profile.show', ['username' => $user->username]) }}"
-                            class="group flex items-center gap-3 rounded-2xl border border-slate-900 bg-slate-950 bg-opacity-80 p-4 transition-colors hover:bg-slate-900"
-                            wire:navigate
-                        >
+                    <li
+                        data-parent=true
+                        x-data="clickHandler"
+                        x-on:click="handleNavigation($event)"
+                    >
+                        <div class="group flex items-center gap-3 rounded-2xl border border-slate-900 bg-slate-950 bg-opacity-80 p-4 transition-colors hover:bg-slate-900">
                             <figure class="{{ $user->is_company_verified ? 'rounded-md' : 'rounded-full' }} h-12 w-12 flex-shrink-0 overflow-hidden bg-slate-800 transition-opacity group-hover:opacity-90">
                                 <img
                                     class="{{ $user->is_company_verified ? 'rounded-md' : 'rounded-full' }} h-12 w-12"
@@ -36,9 +36,14 @@
                                     alt="{{ $user->username }}"
                                 />
                             </figure>
-                            <div class="flex flex-col overflow-hidden text-sm">
-                                <div class="flex items-center space-x-2">
-                                    <p class="truncate font-medium">
+                            <div class="flex flex-col overflow-hidden text-sm text-left">
+                                <a
+                                    class="flex items-center space-x-2"
+                                    href="{{ route('profile.show', ['username' => $user->username]) }}"
+                                    wire:navigate
+                                    x-ref="parentLink"
+                                >
+                                    <p class="text-wrap truncate font-medium">
                                         {{ $user->name }}
                                     </p>
 
@@ -53,12 +58,18 @@
                                             class="size-4"
                                         />
                                     @endif
-                                </div>
+                                </a>
                                 <p class="truncate text-slate-500 transition-colors group-hover:text-slate-400">
                                     {{ '@'.$user->username }}
                                 </p>
                             </div>
-                        </a>
+                            <x-follow-button
+                                :id="$user->id"
+                                :isFollower="auth()->check() && $user->is_follower"
+                                :isFollowing="auth()->check() && $user->is_following"
+                                class="ml-auto"
+                            />
+                        </div>
                     </li>
                 @endforeach
             </ul>
